472a8c20afa385839bad91b9a8d83b583feba641
Aligheri <ivanyevsieiev@gmail.com>
2025-01-15 15:30:27 +0200

    feat : added TopicRequest.java and TopicResponse.java

--- MLF_back/src/main/java/com/mlf_project/topic/TopicRequest.java
+++ MLF_back/src/main/java/com/mlf_project/topic/TopicRequest.java
@@ -1,4 +1,20 @@
 package com.mlf_project.topic;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TopicRequest {
+
+    @NotBlank(message = "Topic name cannot be blank")
+    private String name;
+
+    @NotNull(message = "learningPathID cannot be null")
+    private Long learningPathId;
+
+    private Long parentId;
 }
--- MLF_back/src/main/java/com/mlf_project/topic/TopicResponse.java
+++ MLF_back/src/main/java/com/mlf_project/topic/TopicResponse.java
@@ -1,4 +1,20 @@
 package com.mlf_project.topic;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class TopicResponse {
+    private Long id;
+    private String name;
+    private Long learningPathId;
+    private Long parentId; // Может быть null, если тема является корневой
+    private List<Long> childrenIds; // Список ID дочерних тем
+    private List<Long> articleIds; // Список ID статей, связанных с этой темой
 }
