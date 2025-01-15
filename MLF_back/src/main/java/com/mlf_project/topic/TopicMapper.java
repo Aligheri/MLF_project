4fe04567eb468a026ec114fade300903e37bcde5
Aligheri <ivanyevsieiev@gmail.com>
2025-01-15 15:31:18 +0200

    feat : added TopicMapper.java

--- MLF_back/src/main/java/com/mlf_project/topic/TopicMapper.java
+++ MLF_back/src/main/java/com/mlf_project/topic/TopicMapper.java
@@ -1,7 +1,29 @@
 package com.mlf_project.topic;
 
+import com.mlf_project.article.Article;
+import com.mlf_project.learningPath.LearningPath;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 public class TopicMapper {
+
+    public Topic toTopic(TopicRequest request, LearningPath learningPath, Topic parent) {
+        return new Topic(
+                request.getName(),
+                learningPath,
+                parent
+        );
+    }
+    public TopicResponse toTopicResponse(Topic topic) {
+        return new TopicResponse(
+                topic.getId(),
+                topic.getName(),
+                topic.getLearningPath().getId(),
+                topic.getParent() != null ? topic.getParent().getId() : null,
+                topic.getChildren().stream().map(Topic::getId).collect(Collectors.toList()),
+                topic.getArticles().stream().map(Article::getId).collect(Collectors.toList())
+        );
+    }
 }
